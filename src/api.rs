bc2bb28620a76f1a1779e749a24b1d109565eeb8
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2024-02-25 16:30:11 +0100

    Some additions

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,9 @@
 use crate::params::{PUBLICKEYBYTES, SECRETKEYBYTES, SIGNBYTES};
 use crate::sign::*;
-
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+use crate::SEEDBYTES;
+use zeroize::{Zeroize, ZeroizeOnDrop};
+use constant_time_eq::constant_time_eq;
+#[derive(Clone, PartialEq, Eq, Hash, Zeroize, ZeroizeOnDrop)]
 pub struct Keypair {
   pub public: [u8; PUBLICKEYBYTES],
   secret: [u8; SECRETKEYBYTES],
@@ -30,7 +32,34 @@ impl Keypair {
   pub fn expose_secret(&self) -> &[u8] {
     &self.secret
   }
-
+  /// Check two keys secret (that they are identical)
+  /// ```
+  /// # use pqc_dilithium::*;
+  /// let keys = Keypair::generate();
+  /// let keys2 = Keypair::generate();
+  /// assert_eq!(false,keys.compare_secrets(&keys2));
+  /// assert_eq!(true,keys.compare_secrets(&keys));
+  /// ```
+  pub fn compare_secrets(&self, keys: &Keypair) -> bool {
+    constant_time_eq(&self.secret,&keys.secret)
+  }
+  /// Generate key with random
+  /// ```
+  /// # use pqc_dilithium::*;
+  /// let seed = [3u8; SEEDBYTES];
+  /// let keys = Keypair::generate_with_seed(seed);
+  /// let keys2 = Keypair::generate_with_seed(seed);
+  /// let seed2 = [4u8; SEEDBYTES];
+  /// assert_eq!(true,keys.compare_secrets(&keys2));
+  /// let keys3 = Keypair::generate_with_seed(seed2);
+  /// assert_eq!(false,keys.compare_secrets(&keys3));
+  /// ```
+  pub fn generate_with_seed(seed: [u8; SEEDBYTES]) -> Keypair {
+    let mut public = [0u8; PUBLICKEYBYTES];
+    let mut secret = [0u8; SECRETKEYBYTES];
+    crypto_sign_keypair(&mut public, &mut secret, Some(&seed));
+    Keypair { public, secret }
+  }
   /// Generates a keypair for signing and verification
   ///
   /// Example:
